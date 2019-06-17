5e38660d46b0fb99579fc0b71bc12bc8178f673e
hjlarry <hjlarry@163.com>
2019-06-17 16:10:28 +0800

    add edit list value

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def set_getter(conn, key):
 def list_getter(conn, key):
     return [
         (index, _decode_bytes(value))
-        for index, value in enumerate(conn.lrange(key, start=0, end=-1), 1)
+        for index, value in enumerate(conn.lrange(key, start=0, end=-1))
     ]
 
 
@@ -321,6 +321,15 @@ def list_add_value(db, key):
     elif int(request.form["position"]) == -1:
         conn.rpush(ori_key, request.form["value"])
     return jsonify({"code": 0, "data": url_for("key_detail", db=db, key=key)})
+
+
+@app.route("/db/<db>/<key>/list_edit", methods=["POST"])
+def list_edit_value(db, key):
+    conn.execute_command("SELECT", db)
+    ori_key = parse.unquote_plus(key)
+    index = request.args.get("index", type=int, default=0)
+    conn.lset(ori_key, index, request.form["value"])
+    return redirect(url_for("key_detail", db=db, key=key))
 
 
 @app.route("/db/<db>/<key>", methods=["GET", "POST"])
