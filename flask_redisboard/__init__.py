b2e4a9cf9c80302f968d95ad2900d9839c19c671
hjlarry <hjlarry@163.com>
2019-07-10 14:52:18 +0800

    add must set flask secret_key

--- flask_redisboard/__init__.py
+++ flask_redisboard/__init__.py
@@ -13,6 +13,12 @@ class RedisBoardExtension:
     def init_app(self, app):
         for k, v in self._default_config().items():
             app.config.setdefault(k, v)
+
+        if not app.config.get("SECRET_KEY"):
+            raise RuntimeError(
+                "The Flask-RedisBoard requires the 'SECRET_KEY' config var to be set"
+            )
+
         app.jinja_env.filters["quote_plus"] = url_quote_plus
         app.register_blueprint(module, url_prefix="/redisboard")
 
