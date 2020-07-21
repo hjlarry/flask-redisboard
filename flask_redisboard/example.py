2713ecf4cce48206212c020d54616b26b4d79b61
hjlarry <hjlarry@163.com>
2020-07-21 10:23:10 +0800

    abstract the create_app method

--- flask_redisboard/example.py
+++ flask_redisboard/example.py
@@ -3,21 +3,25 @@ from flask import Flask, redirect, Response
 from flask_redisboard import RedisBoardExtension
 
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = "this is a key"
-app.config["DEBUG"] = True
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "this is a key"
+    app.config["DEBUG"] = True
 
-board = RedisBoardExtension(app)
+    board = RedisBoardExtension()
+    board.init_app(app)
 
+    @app.route("/")
+    def index() -> Response:
+        return redirect("/redisboard/dashboard", code=302)
 
-@app.route("/")
-def index() -> Response:
-    return redirect("/redisboard/dashboard", code=302)
+    return app
 
 
 @click.command()
 @click.option("--host", default="127.0.0.1", help="example site host")
 @click.option("--port", default=6999, help="example site port")
 def main(host: str, port: int) -> None:
-    click.echo(f"redisboard example is run on port: {port}")
+    click.echo(f"redisboard example is run on: http://{host}:{port}")
+    app = create_app()
     app.run(host=host, port=port)
