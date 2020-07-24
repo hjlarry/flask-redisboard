c8a39038421b73d5246591a18635c7f0534ea3a7
hjlarry <hjlarry@163.com>
2020-07-24 09:47:41 +0800

    add key and get key unittest

--- tests/test_views.py
+++ tests/test_views.py
@@ -48,3 +48,48 @@ def test_db(client):
     assert res["code"] == 0
     assert "/redisboard/db/" in res["data"]
 
+
+def test_add_key(client):
+    str_test = dict(type="string", keyname="a", value="astring")
+    list_test = dict(type="list", keyname="b", value="1,3335,5")
+    hash_test = dict(type="hash", keyname="c", index="hashkey", value="hashvalue")
+    set_test = dict(type="set", keyname="d", value="1919,3,1919")
+    zset_test = dict(type="zset", keyname="e", index="1233", value="zsetvalue")
+
+    rv = client.post("/redisboard/db/2/addkey", data=str_test)
+    assert rv.status_code == 302
+
+    rv = client.post("/redisboard/db/2/addkey", data=list_test)
+    assert rv.status_code == 302
+
+    rv = client.post("/redisboard/db/2/addkey", data=hash_test)
+    assert rv.status_code == 302
+
+    rv = client.post("/redisboard/db/2/addkey", data=set_test)
+    assert rv.status_code == 302
+
+    rv = client.post("/redisboard/db/2/addkey", data=zset_test)
+    assert rv.status_code == 302
+
+
+def test_get_key(client):
+    rv = client.get("/redisboard/db/2/a")
+    assert rv.status_code == 200
+    assert b"astring" in rv.data
+
+    rv = client.get("/redisboard/db/2/b")
+    assert rv.status_code == 200
+    assert b"3335" in rv.data
+
+    rv = client.get("/redisboard/db/2/c")
+    assert rv.status_code == 200
+    assert b"hashvalue" in rv.data
+
+    rv = client.get("/redisboard/db/2/d")
+    assert rv.status_code == 200
+    assert b"1919" in rv.data
+
+    rv = client.get("/redisboard/db/2/e")
+    assert rv.status_code == 200
+    assert b"zsetvalue" in rv.data
+    assert b"1233" in rv.data
